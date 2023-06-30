c3e70af526b80cb8a688749f18b104a6204b16c9
abhijitmondal121 <mondalavi9836@gmail.com>
2023-06-30 13:39:23 +0530

    30

--- search.php
+++ search.php
@@ -5,7 +5,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
